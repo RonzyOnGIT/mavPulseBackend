86d38bad9b99887643813aaf59f4d2e3af2c12ab
RonzyOnOF <ninjakai25@gmail.com>
2025-10-09 21:48:14 -0500

    move python files to app folder, create basic Flask instance

---
+++ app/__init__.py
@@ -0,0 +1,16 @@
+# application factory
+from flask import Flask
+
+
+# application factory function
+def create_app(test_config = None) -> Flask:
+    # create and config app
+    app = Flask(__name__)
+
+    # To Do: initialize database with app
+
+    return app
+
+
+    
+
